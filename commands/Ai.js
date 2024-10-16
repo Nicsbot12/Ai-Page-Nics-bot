6f875a3c3f139bb4e5303c53f228c8718e2aff79
Nicsbot12 <154717275+Nicsbot12@users.noreply.github.com>
2024-10-16 21:27:43 +0800

    Update Ai.js

--- commands/Ai.js
+++ commands/Ai.js
@@ -7,7 +7,7 @@ module.exports = {
   async execute(senderId, args, pageAccessToken, sendMessage) {
     const prompt = args.join(' ');
     try {
-      const apiUrl = `https://nics-api.onrender.com/api/chatgpt?question=${encodeURIComponent(prompt)}&uid=100${senderId}`;
+      const apiUrl = `https://markdevs-last-api.onrender.com/gpt4?prompt=${encodeURIComponent(prompt)}&uid=100${senderId}`;
       const response = await axios.get(apiUrl);
       const text = response.data.content;
 
